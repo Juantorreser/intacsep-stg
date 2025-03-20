766b3fd68789d0c6d5e71a99addd6f9f6f9786e0
Juantorreser <jdtorreserazo@gmail.com>
2025-03-20 12:36:40 -0700

    Updated oldBitacoras Count

--- client/src/components/Bitacoras/BitacoraRouter.jsx
+++ client/src/components/Bitacoras/BitacoraRouter.jsx
@@ -35,7 +35,7 @@ const BitacoraRouter = ({edited}) => {
   if (loading) return <p>Loading...</p>;
   if (!bitacora) return <p>Error: Bitácora no encontrada</p>;
 
-  return bitacora.bitacora_id < 350 ? (
+  return bitacora.bitacora_id < 2029 ? (
     <OldBitacoraDetailPage edited={edited} />
   ) : (
     <BitacoraDetailPage edited={edited} />
