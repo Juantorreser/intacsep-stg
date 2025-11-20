6eb5dca26ef3198e3b30bc544aa52d0a1e2156b0
Juantorreser <jdtorreserazo@gmail.com>
2025-11-19 23:55:59 -0800

    V updated

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         className="opacity-25 text-white mb-0 p-0">
         Powered by © Spotynet 2025 on AWS
       </a>
-      <p className="opacity-25 text-white mb-0 footer p-0">v 2.1.7</p>
+      <p className="opacity-25 text-white mb-0 footer p-0">v 2.1.8</p>
     </footer>
   );
 };
